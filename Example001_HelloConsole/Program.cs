c86f2fe28669ac14778e2996f44c853ec32963a4
MNxEN <135325130+MNxEN@users.noreply.github.com>
2024-06-11 21:22:42 +0500

    Update Program.cs

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("2 Hello, World!");
 ﻿Console.WriteLine("Hello, Kseniya!");
 Console.WriteLine("Godd By, Kseniya!");
+Console.WriteLine("fmokdsofgodignoingongoerngoieroigeroigoering");
